788b7a3525b34bae6e0b4836c7eb1ea4914c1066
mixxxxxer <painliader@gmail.com>
2022-10-03 14:06:15 +0000

    Change print of students

--- directory.rb
+++ directory.rb
@@ -1,18 +1,19 @@
+student_list = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+  ]
 #print list of students
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+student_list.each { |student| puts student }
 #print total data
-print "Overall, we have "
-print 11
-puts " great students"
+puts "Overall, we have #{student_list.size} great students"
