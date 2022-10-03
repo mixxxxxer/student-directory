8cc5408f16e47db1dc6200657f565e44b853ff14
mixxxxxer <painliader@gmail.com>
2022-10-03 17:17:58 +0000

    Add new features to method

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+=begin
 students = [
   { name: "Dr. Hannibal Lecter", cohort: :november },
   { name: "Darth Vader", cohort: :november },
@@ -11,13 +12,42 @@ students = [
   { name: "Joffrey Baratheon", cohort: :november },
   { name: "Norman Bates", cohort: :november }
   ]
+=end
+def input_student
+  students = []
+  puts "Please, add name"
+  puts "Enter twice instead of a name, if you finish."
+  name = gets.chomp
+  puts "Please, add date of birthday (11.11.1111)"
+  date = gets.chomp
+  puts "Please, cohort"
+  cohort = gets.chomp
+  while !name.empty? do
+    students << {name: name, date: date, cohort: cohort}
+    puts "Now we have #{students.count} students"
+    puts "Please, add name"
+    name = gets.chomp
+    if !name.empty? 
+      puts "Please, add date of birthday (11.11.1111)"
+      date = gets.chomp
+      puts "Please, cohort"
+      cohort = gets.chomp
+    end
+  end
+  students
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
 def print_array(student_list)
-  student_list.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
+  student_list.each_with_index { |student, id| 
+    #if student[:name].length >= 12 // print only students, whose name longer then 12
+    #if student[:name][0].downcase == "a" // print students, whose name starts from "a"
+    puts "#{id + 1}.#{student[:name].ljust(30)} #{student[:date].ljust(10)} #{("(" + student[:cohort]).rjust(15)} cohort)" 
+  }
 end
 
 def print_footer(student_list)
@@ -25,5 +55,6 @@ def print_footer(student_list)
 end
 
 print_header
+students = input_student
 print_array(students)
 print_footer(students)
